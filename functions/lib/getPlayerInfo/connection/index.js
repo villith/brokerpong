d1577d275b798807aa8fc138ae94256337b2ca64
Scott Donnelly <scoot.donnelly@gmail.com>
2019-09-17 21:10:30 -0400

    add proper generate dfiles

--- functions/lib/getPlayerInfo/connection/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-exports.default = mongoose.connect(process.env.MONGO_URL);
-//# sourceMappingURL=index.js.map
