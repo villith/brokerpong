8b6501b24942a71b950a8b763e7a235ec1e00202
Scott Donnelly <scoot.donnelly@gmail.com>
2019-09-21 19:47:00 -0400

    try cloudbuild for functions

--- functions/lib/helloworld/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const helloHttp = (req, res) => res.send("hello world");
-exports.helloHttp = helloHttp;
-//# sourceMappingURL=index.js.map
